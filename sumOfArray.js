8adb5dbd29921bf1c799dac6ec7768c6814977fd
devmunira <fswfofficial@gamil.com>
2023-10-11 08:08:31 +0600

    Done sum of array integer

--- sumOfArray.js
+++ sumOfArray.js
@@ -1,6 +1,6 @@
 // Sum all integer elements of an array
 
-const sumOfArray = (array) => {
+const sumOfArray = (array = []) => {
     return array.reduce((acc,cur) => {
         if(typeof cur === 'number') acc += cur
         return acc;
